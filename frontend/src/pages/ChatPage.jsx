5170e98beea71493f721049f08012d0e29583a61
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-21 10:21:44 +0200

    add routing and login form

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,15 @@
+
+const ChatPage = () => {
+
+  return (
+    <>
+      <div>
+        <p>
+          Main Page
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import notFoundImage from '../assets/not_found.svg'
+
+const PageNotFound = () => (
+  <div className="text-center">
+    <img
+      alt="Страница не найдена"
+      className="img-fluid h-25"
+      src={notFoundImage}
+      style={{ width: '200px' }}
+    />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      {' '}
+      <Link to="/">на главную страницу</Link>
+    </p>
+  </div>
+)
+
+export default PageNotFound
