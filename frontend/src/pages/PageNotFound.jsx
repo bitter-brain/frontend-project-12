7939fd99e174d42ff25a803930b9b51fe8ff70e6
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-28 18:16:14 +0200

    remove image size styles from component

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -7,7 +7,6 @@ const PageNotFound = () => (
       alt="Страница не найдена"
       className="img-fluid h-25"
       src={notFoundImage}
-      style={{ width: '200px' }}
     />
     <h1 className="h4 text-muted">Страница не найдена</h1>
     <p className="text-muted">
