5407a564a0d29542e15d6a0227bcd1ade48b0cdc
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-28 23:01:59 +0200

    add spinner loading component

---
+++ frontend/src/components/SpinnerLoading.jsx
@@ -0,0 +1,11 @@
+import { Spinner } from 'react-bootstrap'
+
+const SpinnerLoading = () => {
+  return(
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <Spinner animation="border" />
+    </div>
+  )
+}
+
+export default SpinnerLoading
